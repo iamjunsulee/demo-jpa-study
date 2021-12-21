260891d23846773f09432c94a1f4d2d100a916c4
iamjunsulee <mellow0924@gmail.com>
2021-12-21 15:15:41 +0900

    vuetify 적용

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,42 +1,19 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link to="/members" class="nav-link">회원목록</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/add" class="nav-link">회원등록</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/addItem" class="nav-link">상품등록</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/items" class="nav-link">상품목록</router-link>
-        </li>
-      </div>
-    </nav>
-
-    <div class="container mt-3">
+  <v-app>
+    <v-app-bar app dark>
+      <v-btn to="/join" text>Join</v-btn>
+      <v-btn to="/members" text>Members</v-btn>
+      <v-btn to="/addItem" text>Register</v-btn>
+      <v-btn to="/items" text>Items</v-btn>
+    </v-app-bar>
+    <v-main>
       <router-view />
-    </div>
-  </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App'
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
